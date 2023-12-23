43f49437415abbf09e34f8d51bed282879019f3c
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-23 20:36:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello World!')
+// })
+
+// we can write in either ways
+app.get('/',function(req,res){
+    res.send('Hello World!')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Example app is listening on ${port}`)
+});
