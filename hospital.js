b7a707412aa4122e4a29fa192b1274fda8844fcf
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-26 12:34:12 +0530

    put method

--- hospital.js
+++ hospital.js
@@ -4,39 +4,39 @@ const app = express();
 const port = 3000;
 
 //global mini-database where users stores data;
-let users = [
+var users = [
   {
     name: "jhon",
     kidneys: [
       {
         healthy: false,
       },
-    //   {
-    //     healthy: true,
-    //   },
+      //   {
+      //     healthy: true,
+      //   },
     ],
   },
 ];
 
 /**
- * get parameters used to fetch data;
+ * GET parameters used to fetch data;
  * we can also use query param for data;
  */
 
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  const jhonKidneys = users[0].kidneys;
-  const numberOfKidneys = jhonKidneys.length;
-  const numberOfHealthyKidneys = 0;
+  let jhonKidneys = users[0].kidneys;
+  let numberOfKidneys = jhonKidneys.length;
+  let numberOfHealthyKidneys = 0;
 
-  for (const i = 0; i < jhonKidneys.length; i++) {
+  for (let i = 0; i < jhonKidneys.length; i++) {
     if (jhonKidneys[i].healthy) {
       numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
     }
   }
 
-  const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+  let numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
 
   res.json({
     numberOfKidneys,
@@ -45,24 +45,42 @@ app.get("/", function (req, res) {
   });
 });
 
-app.post("/",function(req,res){
-    console.log(req.body);
-    const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push({
-        healthy: isHealthy
-    })
-    res.json({
-        msg: "Done!"
-    })
+/**
+ * POST is used to post the data;
+ * it is done through postman server;
+ * app.use(express.json()) should be used;
+ */
+app.post("/", function (req, res) {
+  console.log(req.body);
+  let isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  });
+});
+
+
+/**
+ * PUT method is modify data to a certain level...
+ * PATCH is used to modify a patch of data like put not whole content;
+ */
+app.put("/", function (req, res) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({});
+});
+
+/**
+ * DELETE method is used to delete data
+ */
+
+
+app.delete("/",function(req,res){
+
 })
-
-// app.delete("/",function(req,res){
-
-// })
-
-// app.put("/",function(){
-
-// })
 
 app.listen(port, function () {
   console.log(`app listening  ${port}`);
