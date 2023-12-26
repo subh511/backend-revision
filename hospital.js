f0770630842c1472234d38ea769a7da48d04d600
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-26 12:21:55 +0530

    post method

--- hospital.js
+++ hospital.js
@@ -3,7 +3,8 @@ const app = express();
 
 const port = 3000;
 
-const users = [
+//global mini-database where users stores data;
+let users = [
   {
     name: "jhon",
     kidneys: [
@@ -17,12 +18,19 @@ const users = [
   },
 ];
 
+/**
+ * get parameters used to fetch data;
+ * we can also use query param for data;
+ */
+
+app.use(express.json());
+
 app.get("/", function (req, res) {
   const jhonKidneys = users[0].kidneys;
   const numberOfKidneys = jhonKidneys.length;
   const numberOfHealthyKidneys = 0;
 
-  for (let i = 0; i < jhonKidneys.length; i++) {
+  for (const i = 0; i < jhonKidneys.length; i++) {
     if (jhonKidneys[i].healthy) {
       numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
     }
@@ -37,9 +45,16 @@ app.get("/", function (req, res) {
   });
 });
 
-// app.post("/",function(req,res){
-
-// })
+app.post("/",function(req,res){
+    console.log(req.body);
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
 
 // app.delete("/",function(req,res){
 
