e721efe4ea815c032fa2329cfc6637c07cfa090d
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-27 21:11:57 +0530

    logics

--- logic/logic.js
+++ logic/logic.js
@@ -1,28 +1,10 @@
 let users = [
-    {
-      name1: "jhon",
-      kidneys: [
-        {
-          healthy: false,
-        },
-      ],
-    }
-  ];
-
-//totalKidneys;
-//healthyKidneys;
-//unhealthyKidneys;
-
- let jhonKidneys = users[0].kidneys;
- let numberOfKidneys = jhonKidneys.length;
- let numberOfHealthyKidneys = 0;
-
-for(let i=0; i<jhonKidneys.length; i++){
-    if(jhonKidneys[i].healthy){
-        numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
-    }
-}
-
-let numberOfUnhealthyKidneys = numberOfHealthyKidneys - numberOfKidneys;
-
-console.log(numberOfUnhealthyKidneys)
+  {
+    name1: "jhon",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
