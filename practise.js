159bc3879c97169f5f91e15152718c9ff5d10199
subhranshu sekher dhal <ssdhal98@gmail.com>
2023-12-27 20:27:17 +0530

    practise.js

---
+++ practise.js
@@ -0,0 +1,67 @@
+// const express = require("express");
+
+// const app = express();
+
+// const port = 3000;
+
+// let arr = [1,2,3,4,5]
+// function even(n){
+//     for(let i=0; i<=arr.length; i++){
+//         if(arr[i] % n == 0){
+//             return true
+//         }else{
+//             return false
+//         }
+// }
+// }
+// app.get("/",function(req,res){
+//     let calculate = req.query.n;
+//     let ans = even(calculate);
+//     res.send("your answer is: " + ans)
+// })
+
+// app.listen(port,function(){
+//     console.log(`app listening on ${port}`)
+// })
+
+const express = require("express");
+
+const port = 3000;
+
+const app = express();
+
+let users = [
+  {
+    name: "jhon",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
+
+app.get("/", function (req, res) {
+
+  let jhonKidneys = users[0].kidneys;
+  let numberOfKidneys = jhonKidneys.length;
+  let numberHealthyKidney = 0;
+
+  for(let i=0; i<jhonKidneys.length; i++){
+    if(jhonKidneys[i].length){
+      numberHealthyKidney = numberHealthyKidney + 1;
+    }
+  }
+
+  let numberOfUnhealthyKidneys = numberOfKidneys - numberHealthyKidney
+
+  res.json({
+    numberOfUnhealthyKidneys,
+    numberOfKidneys,
+    numberHealthyKidney
+  })
+});
+
+app.listen(port, function () {
+  console.log(`app listening on port ${port}`);
+});
