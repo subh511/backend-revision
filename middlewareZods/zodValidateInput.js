737f4a29d63e083003150fafc636f5df27737279
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-01-13 12:42:12 +0530

    zod schema ex with array

---
+++ middlewareZods/zodValidateInput.js
@@ -0,0 +1,15 @@
+const zod = require("zod");
+
+
+function validateInput(arr){
+    const schema = zod.array(zod.number());
+    const response = schema.safeParse(arr);
+    console.log(response);
+}
+
+validateInput([1,2,3])
+//schema validation will be sucess;
+
+validateInput(["1",2,3])
+//schema validation will be unsucess due a string present;
+//over in the array;
