ee4484e243959ff8f8b16f61eae8dbe5a5e21060
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-01-16 11:14:25 +0530

    ride1 modification  done

--- middlewareZods/middleware_recap.js
+++ middlewareZods/middleware_recap.js
@@ -4,19 +4,27 @@ const app = express();
 const port = 3000;
 
 //introduce a function whose age is atleast 14yrs old;
-function isOldEnough(age){
-    if(age>=14){
-        return true;
-    }else{
-        return false;
-    }
+function isOldEnough(age) {
+  if (age >= 14) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-//prog-1;
+//prog-1; 
+//prog-2; modified & introduced query Function;
+
 app.get("/ride1", function (req, res) {
-  res.json({
-    msg: "you have successfully ridden the ride",
-  });
+  if (isOldEnough(req.query.age)) {
+    res.json({
+      msg: "you have successfully ridden the ride",
+    });
+  } else {
+    res.status(411).json({
+      msg: "you are under age!",
+    });
+  }
 });
 
 app.post("/", function (req, res) {});
